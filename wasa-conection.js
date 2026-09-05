ad83a46d8264da6dc16f9a2c40e120237b091a2a
ironic595 <ironic595@gmail.com>
2026-09-05 01:10:38 -0300

    Update price tiers in WASA_CONFIG

--- wasa-conection.js
+++ wasa-conection.js
@@ -3,7 +3,7 @@ const WASA_CONFIG = {
   USDT_CONTRACT: '0x55d398326f99059fF775485246999027B3197955',
   RECEIVER: null,
   WORKER_URL: 'https://games-wasa-worker.javimsites.workers.dev/',
-  PRICES: { 10: 2, 50: 10, 100: 19 },
+  PRICES: { 200: 2, 1000: 10, 2000: 19 },
   _loaded: false
 };
 let connectedWallet = null; let connectedNickname = null; let connectedEmail = null;
